b393974adbbd762111b6e41aa640818b47bada40
pangpangmandu <srbin9444@gmail.com>
2020-08-27 19:02:48 +0900

    first commit

--- diary/views.py
+++ diary/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("hi")
+    return render(request, 'diary/index.html',{})
     
 # Create your views here.
